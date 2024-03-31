2743c8c80090c53cc2b501aef92290fe6a52d635
Mithilesh12255 <165554273+Mithilesh12255@users.noreply.github.com>
2024-03-31 15:09:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.Image.FromURL('BirthdayImage.jpg', function(Img) {
+block.image.object = Img;
+
+
+block.image.object.scaleToWidth(700);
+block.image.object.scaleToHeight(510);
+block.image.object.set({
+    top:0,
+    left:0
+});
+<canvas.add(block_image_object);
+    )};
+
+
+function playSound(){
+	onclick = bdayaaudio.mp3.play();
+}
